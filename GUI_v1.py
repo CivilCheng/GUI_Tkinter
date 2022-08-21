3678bc100a1970a6801825ca586f962d2d379815
CivilCheng <66505110+CivilCheng@users.noreply.github.com>
2022-08-20 22:59:14 -0400

    Update GUI_v1.py

--- GUI_v1.py
+++ GUI_v1.py
@@ -6,13 +6,17 @@ Created on Mon Aug 15 23:32:52 2022
 """
 
 import os
+import pandas as pd
 from tkinter import filedialog
 from tkinter import *
+from matplotlib.figure import Figure
+from matplotlib.backends.backend_tkagg import (FigureCanvasTkAgg, 
+NavigationToolbar2Tk)
 
 def browse_button():
-    filename = filedialog.askdirectory()
-    e1_value.set(filename)
-    return filename
+    path = filedialog.askdirectory()
+    e1_value.set(path)
+    return path
 
 def list_filename():
     listbox1.delete(0, END)
@@ -21,9 +25,71 @@ def list_filename():
     for i in name_list:
         listbox1.insert(END, i)
     
+def getElement(event):
+    selection = event.widget.curselection()
+    if selection:
+        
+        #get file name
+        index = selection[0]
+        name = event.widget.get(index)
+        lb3_value.set(name)
+        
+        #import data
+        global data
+        path_name = os.path.join(e1_value.get(), name)
+        data = pd.read_csv(path_name)
+        
+        listbox2.delete(0, END)
+        for i in data.columns:
+            listbox2.insert(END, i)
+        
+    else:
+        lb3_value.set('')
+
+def plot(event):
+    global data
+    
+    selection = event.widget.curselection()
+    if selection:
+        
+        #get file name
+        index = selection[0]
+        col = event.widget.get(index)
+        
+        print(data)
+        
+        # the figure that will contain the plot
+        fig = Figure(figsize = (2, 2),
+                      dpi = 100)
+              
+        # adding the subplot
+        plot1 = fig.add_subplot(111)
+      
+        # plotting the graph
+        plot1.plot(data[col])
+      
+        # creating the Tkinter canvas
+        # containing the Matplotlib figure
+        canvas = FigureCanvasTkAgg(fig, master = window)  
+        canvas.draw()
+      
+        # placing the canvas on the Tkinter window
+        canvas.get_tk_widget().grid(row=4, column=1, columnspan=2)
+      
+        # creating the Matplotlib toolbar
+        # toolbar = NavigationToolbar2Tk(canvas, window)
+        # toolbar.update()
+      
+        # placing the toolbar on the Tkinter window
+        # canvas.get_tk_widget().grid(row=4, column=1, columnspan=2)    
+    
+    
 
 window = Tk()
 window.geometry('600x400')
+
+dataset = IntVar()
+
 
 lb1 = Label(window, text='Path')
 lb1.grid(row=0, column=0)
@@ -35,25 +101,33 @@ e1 = Entry(window, textvariable=e1_value)
 e1.grid(row=0, column=1)
 
 
-listbox1 = Listbox(window, width=20, height=10, selectmode=SINGLE)
+listbox1 = Listbox(window, width=30, height=10, selectmode=SINGLE)
 
 button2 = Button(text="Open", command=list_filename).grid(row=0, column=4)
 
 listbox1.grid(row=1, column=1, columnspan=2)
+listbox1.bind('<<ListboxSelect>>', getElement)
 
 scrollbar1 = Scrollbar(window)
 scrollbar1.grid(row=1, column=3, sticky="NS")
 scrollbar1.config( command = listbox1.yview )
 
+lb2 = Label(window, text='Name: ')
+lb2.grid(row=2, column=0)
 
+lb3_value = StringVar()
+lb3 = Label(window, textvariable=lb3_value)
+lb3.grid(row=2, column=1)
 
+lb4 = Label(window, text='Column: ')
+lb4.grid(row=3, column=0)
 
+listbox2 = Listbox(window, width=30, height=5, selectmode=SINGLE)
+listbox2.grid(row=3, column=1, columnspan=2)
+listbox2.bind('<<ListboxSelect>>', plot)
 
 
 
 
 
 window.mainloop()
-
-
-
